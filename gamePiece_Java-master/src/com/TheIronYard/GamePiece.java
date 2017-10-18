0fa00944884dd36aa7140cbdb714a0a34e2a3da8
Tyler Jurecki <jurecki07@yahoo.com>
2017-10-18 09:41:34 -0500

    initial commit

---
+++ gamePiece_Java-master/src/com/TheIronYard/GamePiece.java
@@ -0,0 +1,56 @@
+package com.TheIronYard;
+
+public class GamePiece {
+    int positionX = 0;
+    int positionY = 0;
+    boolean frozen = false;
+    String name;
+    String color;
+
+    public int getPositionX() { return positionX; }
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+    public int getPositionY() {
+        return positionY;
+    }
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+    public boolean isFrozen() {
+        return frozen;
+    }
+    public void setFrozen(boolean frozen) {
+        this.frozen = frozen;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public GamePiece() { this.name = name; this.color = color;}
+
+
+    void move(int positionX, int positionY){
+        if(positionX<= 0 && positionY>= 0 {
+            this.positionX = positionX;
+            this.positionY = positionY;
+        }else System.out.println("Invalid movement");
+    }
+
+    void freeze(){
+        this.frozen = true;
+    }
+
+    void unfreeze(){
+        this.frozen = false;
+    }
+
+}
---
+++ gamePiece_Java-master/src/com/TheIronYard/Main.java
@@ -0,0 +1,32 @@
+package com.TheIronYard;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GamePiece gamePiece = new GamePiece();
+
+        if (!gamePiece.isFrozen()) {
+            gamePiece.freeze();
+        System.out.println(gamePiece.frozen);
+        }
+        if (gamePiece.isFrozen()) {
+            gamePiece.unfreeze();
+            System.out.println(gamePiece.frozen);
+        }
+
+        if (!gamePiece.isFrozen()) {
+            System.out.println("positionX before moving: " + gamePiece.getPositionX());
+            System.out.println("positionY before moving: " + gamePiece.getPositionY());
+
+            gamePiece.move(3, 4);
+            System.out.println("positionX after moving: " + gamePiece.getPositionX());
+            System.out.println("positionY after moving: " + gamePiece.getPositionY());
+        }
+
+        if (gamePiece.isFrozen()) {
+            System.out.println(gamePiece.move() +" is frozen and can't move");
+        }
+
+    }
+}
